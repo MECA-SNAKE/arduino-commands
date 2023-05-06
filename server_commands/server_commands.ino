ce568327e12004d0dece2e6379c63075e8b2a920
Giovanni Ranieri <gioran2017@gmail.com>
2023-05-06 16:31:07 +0200

    :+1: many updates for motion

--- server_commands/server_commands.ino
+++ server_commands/server_commands.ino
@@ -25,6 +25,10 @@
 // -------------------------------------------------------------------------------------
 typedef Adafruit_PWMServoDriver Driver;
 
+typedef enum {
+  UNDULATED, CONCERTINA, NONE;
+} MOTION;
+
 // -------------------------------------------------------------------------------------
 // PARAMETERS
 // -------------------------------------------------------------------------------------
@@ -35,7 +39,16 @@ const char* password = "21ff99a2c0cd";
 
 AsyncWebServer server(80);
 
-float rot = 0.0;
+int is_running = 0; 
+MOTION motion = MOTION.NONE;
+
+double amplitude = 0.0;
+double offset = 0.0;
+double wavelength = 0.0;
+double frequency = 0.0; 
+
+double rot = 0.0;
+double angles[N_SERVOS - 2];
 
 // -------------------------------------------------------------------------------------
 // MAIN FUNCTIONS
@@ -45,7 +58,7 @@ float rot = 0.0;
 // -------------------------------------------------------------------------------------
 
 // This function rotates a specific servo of an certain angle
-int rotate_with_min_max(int servo, float angle) {
+int rotate_with_min_max(int servo, double angle) {
 
   int pulse_wide = map(angle, 0, 180, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
 
@@ -57,7 +70,7 @@ int rotate_with_min_max(int servo, float angle) {
 // -------------------------------------------------------------------------------------
 
 // This function rotates a specific servo of an certain angle (simpler)
-void rotate(int servo, float angle) {
+void rotate(int servo, double angle) {
   int value = driver.setPWM(servo, 0, rotate_with_min_max(servo, angle));
 }
 
@@ -66,7 +79,7 @@ void rotate(int servo, float angle) {
 
 // This function activates the inchworm's motion (ASSUME 3 SERVOS)
 void inchworm_motion() {
-   for(int phi = 0; phi <90; ++phi){
+   for(int phi = 0; phi < 90; ++phi){
     rotate(0, 90-phi);
     rotate(1, 90-phi);
     rotate(2, 90+phi);
@@ -76,7 +89,7 @@ void inchworm_motion() {
 
   for(int i = 0; i<=4; ++i){
     for(int phi = 0; phi<90; ++phi){
-      if(i>0){
+      if(i > 0){
         rotate(i-1, 180-phi);
       }
 
@@ -90,7 +103,7 @@ void inchworm_motion() {
     delay(1000);
   }
 
-  for(int i =0;i<N_SERVOS; i++){
+  for(int i =0;i < N_SERVOS; i++){
     rotate(i, 90);
     delay(10);
   }
@@ -115,21 +128,120 @@ void concertina_motion() {
 // -------------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------
 
-// This function activates the undulated motion of the snake
-void undulated_motion() {
-  for(int i = 0; i < 360; i++) {
-    for(int j = 0; j < N_SERVOS; j++) {
-      delay(30);
-      if(j > N_SERVOS - 6) { // j > 2
-        rot = 90 + 50 * sin(3 * i + (1 * j * 2 * 3.1415) / (N_SERVOS - 1));
-      } else {
-        rot = 90 + 62 * sin(3 * i + (1 * j * 2 * 3.1415) / (N_SERVOS - 1));
-      }
-      
-      rotate(j, rot);
-    }
+void straight() {
+  for(int i = 0; i < N_SERVOS; i++) {
     delay(100);
+    rotate(i, 90);
   }
+}
+
+// -------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------
+
+float forward_kinetics_2() {
+  float val = 0.0;
+  for(int i = 0; i < N_SERVOS - 2; i++) {
+    val += sin(angles[i]);
+  }
+  return 90.0 - asin(val);
+}
+
+// -------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------
+
+// This function activates the undulated motion of the snake
+void undulated_motion_kinetics() { // NEED TO BE TESTED BETTER MAYBE
+  for(int i = 0; i < 360; i++) {
+    for(int j = 0; j < N_SERVOS; j++) { // n servo - 1
+     // delay(30);
+      rot = 90 + 55 * sin(2 * 2 * 3.1415 * i + (2 * j * 2 * 3.1415) / (N_SERVOS - 1));
+      rotate(j, rot);
+      //angles[j] = rot;
+      //delay(10);
+      //rotate(N_SERVOS - 1, forward_kinetics_2());
+    }
+    delay(10);
+  }
+}
+
+// -------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------
+
+// DIDNT ADD KINETICS BUT WE COULD TRY
+void undulated_motion_3() {
+  for(int i = 0; i < 360; i++) {
+    float brads = i * 3.1415 / 180.0;     //convert from degrees to radians
+    for(int j=0; j<N_SERVOS; j++){  
+        if(is_running == 0) {
+          return;
+        }
+        rotate(j, 90 + 55 * sin(4 * brads + (1 * j * 2 * 3.1415) / (N_SERVOS - 1)));
+    }
+   delay(10);
+  }
+}
+
+float l = 1.0;
+float factor = 0.0;
+
+float amplitude_factor(float ampl, float stop_ampl, float steps) {
+    return pow(ampl / stop_ampl, 1 / steps);
+}    
+
+void undulated_motion_4() {
+  l = 1.0;
+  factor = amplitude_factor(55, 35, N_SERVOS - 1);
+
+  for(int i = 0; i < 360; i++) {
+
+    float brads = i * 3.1415 / 180.0;
+
+    for(int j=0; j<N_SERVOS; j++){  
+        rotate(j, 90 + l * 55 * sin(4 * brads + (1 * j * 2 * 3.1415) / (N_SERVOS - 1))); // need to test with n and not n - 1 maybe for the period
+        l /= factor;
+    }
+
+   delay(10);
+  }
+}
+
+void update_mode(int m) {
+  if(m == 0 || m == 1) {
+    is_running = m;
+  }
+}
+
+void update_amplitude(double amp) {
+  if(amp >= 30 && amp <= 70) {
+    amplitude = amp;
+  }
+}
+
+void update_wavelength(double wl) {
+  if(wl >= 0.5 && wl <= 4) {
+    wavelength = wl;
+  }
+}
+
+void update_frequency(double f) {
+  if(f >= 0.5 && wl <= 4) {
+    frequency = f;
+  }
+}
+
+void update_offset(double off) {
+  if(off >= 0 && off <= 20) {
+    offset = off;
+  }
+}
+
+void reset() {
+  is_running = 0;
+  offset = 0.0;
+  motion = MOTION.NONE;
+  frequency = 1.0;
+  wavelength = 2.0;
+  amplitude = 50.0;
 }
 
 
@@ -139,7 +251,7 @@ void undulated_motion() {
 void setup() {  
 
   Serial.begin(9600); 
-/*
+
   Serial.print("WIFI ...");
   WiFi.begin(ssid, password);
   Serial.print("Connecting to Wifi...");
@@ -156,32 +268,52 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   server.on("/mode", HTTP_POST, [](AsyncWebServerRequest *request){
-    AsyncWebParameter* p = request->getParam(0); // 1: start / 0: stop
-    if(p->isPost()) {
-      Serial.printf("POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
+    if(request->hasParam("value", true)) {
+      AsyncWebParameter* p = request->getParam("value", true);
+      int value = p->value().toInt();
+      Serial.println("value for /mode: %d", value);
 
-      request->send_P(200, "text/html", "post request for /mode success");
+      update_mode(value);
+
+      request->send(200, "text/html", "Received value for /mode on the ESP8266");
+    } else {
+      request->send(404, "text/html", "Error on /mode POST request");
     }
   });
 
   server.on("/motion", HTTP_POST, [](AsyncWebServerRequest *request){
-    //request->send_P(200, "text/html", "Hello from the ESP8266");
+    
+  });
+
+  server.on("/amp", HTTP_POST, [](AsyncWebServerRequest *request){
+    
+  });
+
+  server.on("/freq", HTTP_POST, [](AsyncWebServerRequest *request){
+    
+  });
+
+  server.on("/wl", HTTP_POST, [](AsyncWebServerRequest *request){
+    
+  });
+
+  server.on("/offset", HTTP_POST, [](AsyncWebServerRequest *request){
+    
   });
 
   server.begin();
-*/
+
   Serial.println("");
   Serial.println("Initialize System");
 
   driver.begin();
   driver.wakeup();
-  driver.setPWMFreq(FREQUENCY_SERVO); // which frequency of our servos -> need to look datasheet
+  driver.setPWMFreq(FREQUENCY_SERVO); // which frequency of our servos
 
   // Initialize all servo at their midpoint
-  for(int i = 0; i < N_SERVOS; i++) {
-    delay(100);
-    rotate(i, 90);
-  }
+  straight();
+
+  delay(2000);
 }
 
 // -------------------------------------------------------------------------------------
@@ -189,7 +321,7 @@ void setup() {
 // -------------------------------------------------------------------------------------
 void loop() {
 
-  undulated_motion();
+  undulated_motion_4();
 
   if (WiFi.status() != WL_CONNECTED) {
     Serial.println("Wifi disconnected");
@@ -204,4 +336,46 @@ void loop() {
     Serial.println("Wifi reconnected");
     server.begin();
   }
+
+  if(is_running == 1) {
+    switch(motion){
+
+      case UNDULATED:
+        undulated_motion_3();
+        break;
+
+      case CONCERTINA:
+        concertina_motion();
+        break;
+
+      case NONE:
+        // ...
+        break;
+
+      default:
+        break;    
+
+    }  
+
+  } else {
+    reset();
+  }
+  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
